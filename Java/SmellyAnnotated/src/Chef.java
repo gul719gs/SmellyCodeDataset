79d421c4b334f74b576bec4978de599bb48f1686
Gulafshan Parveen <gul719gs@gmail.com>
2025-12-13 17:43:56 +0530

    Create Chef.java

---
+++ Java/SmellyAnnotated/src/Chef.java
@@ -0,0 +1,14 @@
+public class Chef {
+
+    public Chef() {
+        // Default constructor
+    }
+
+    public void preparePizza() {
+        System.out.println("Chef is preparing pizza");
+    }
+
+    public void prepareDrink() {
+        System.out.println("Chef is preparing drink");
+    }
+}
